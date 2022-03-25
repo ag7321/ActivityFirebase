303a9e5f2b4b7bf8fbfb294fc9cddf889e111f0a
ang <ag888927@gmail.com>
2022-03-24 21:11:58 -0600

    ultimos cambios con Preferences

--- app/src/main/java/com/example/activityfirebase/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/activityfirebase/ui/home/HomeFragment.java
@@ -4,32 +4,68 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
+
+import com.example.activityfirebase.R;
 import com.example.activityfirebase.databinding.FragmentHomeBinding;
+import com.example.activityfirebase.process.Preferences;
 
 public class HomeFragment extends Fragment {
 
 private FragmentHomeBinding binding;
-
+    EditText usuario,password;
+    Button btnCargar, btnSalvar;
+    com.example.activityfirebase.process.Preferences preferences;
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
 
-    binding = FragmentHomeBinding.inflate(inflater, container, false);
-    View root = binding.getRoot();
+            View root = inflater.inflate(R.layout.fragment_home,container,false);
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+            usuario = root.findViewById(R.id.txtUser);
+            password = root.findViewById(R.id.txtClave);
+
+            btnCargar = root.findViewById(R.id.btnload);
+            btnSalvar= root.findViewById(R.id.btnSave);
+
+            btnCargar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    cargarInfo();
+                }
+            });
+
+            btnSalvar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    saveInfo();
+                }
+            });
+
+            preferences = new Preferences();
+            preferences.loadProferences(getContext());
+
         return root;
     }
 
-@Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    private void saveInfo(){
+        preferences.savePreferences(getContext(),usuario.getText().toString(),password.getText().toString());
+        Toast.makeText(getContext(), "Datos Salvados", Toast.LENGTH_SHORT).show();
     }
+
+    private  void cargarInfo(){
+        try {
+            usuario.setText(preferences.getNombre());
+            password.setText(preferences.getPassword());
+        }catch (Exception exception){
+            exception.toString();
+        }
+
+
+    }
+
 }
